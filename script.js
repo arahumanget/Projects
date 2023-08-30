1a0f48c9ba90b7b9ce4604bcfdd3656ba21a6399
arahumanget <113668568+arahumanget@users.noreply.github.com>
2023-08-30 15:02:02 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const addEntryButton = document.getElementById("add-entry");
+    const checklistTable = document.getElementById("checklist");
+    const templateRow = document.getElementById("template-row");
+
+    addEntryButton.addEventListener("click", function () {
+        const newRow = templateRow.cloneNode(true);
+        newRow.removeAttribute("id");
+        newRow.style.display = "table-row";
+        checklistTable.appendChild(newRow);
+    });
+
+    // Handle checkbox logic as before
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    
     checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener("change", function () {
             const row = this.closest("tr");
